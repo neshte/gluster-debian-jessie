53faf81484ec7dfd1e54dad16613b5c25853297d
neshte <elbuenjim@gmail.com>
2016-05-16 10:04:24 -0500

    ups, doing it wrong with the environmental variables

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@
 service glusterfs-server start
 
 mkdir -p /data
-gluster volume create $(GLUSTERVOLNAME) $(hostname):/data force
-gluster volume start $(GLUSTERVOLNAME)
+gluster volume create $GLUSTERVOLNAME $(hostname):/data force
+gluster volume start $GLUSTERVOLNAME
 
 tail -f /var/log/glusterfs/etc-glusterfs-glusterd.vol.log
 
