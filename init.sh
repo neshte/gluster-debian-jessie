87ed8979ee14fbf67e5c4185c69b4bfa35462fa5
neshte <elbuenjim@gmail.com>
2016-05-17 08:24:35 -0500

    changed to only open server and not setup a thing

--- init.sh
+++ init.sh
@@ -3,7 +3,5 @@
 service glusterfs-server start
 
 mkdir -p /data
-gluster volume create $GLUSTERVOLNAME $(hostname):/data force
-gluster volume start $GLUSTERVOLNAME
 
 tail -f /var/log/glusterfs/etc-glusterfs-glusterd.vol.log
